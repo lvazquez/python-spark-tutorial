4f325993b5599297be8f98a1def07eda11e5757f
Luis Vazquez <luar.vazquez@gmail.com>
2023-07-29 21:14:43 -0300

    Cambios en wordcount y en include de ejemplo

--- rdd/WordCount.py
+++ rdd/WordCount.py
@@ -1,15 +1,17 @@
 from pyspark import SparkContext, SparkConf
 
 if __name__ == "__main__":
-    conf = SparkConf().setAppName("word count").setMaster("local[3]")
-    sc = SparkContext(conf = conf)
+    conf = SparkConf().setAppName("word count").setMaster("local[*]")
+    sc = SparkContext(conf=conf)
     
     lines = sc.textFile("in/word_count.text")
     
+    # Transforms
     words = lines.flatMap(lambda line: line.split(" "))
-    
-    wordCounts = words.countByValue()
-    
-    for word, count in wordCounts.items():
-        print("{} : {}".format(word, count))
+    wordPairs = words.map(lambda x: (x, 1))
+    wordCounts = wordPairs.groupByKey().mapValues(len)
 
+    # Action
+    topWords = sorted(wordCounts.collect(), key=lambda k_v: k_v[1], reverse=True)[0:10]
+    for word, count in topWords:
+        print("{} : {}".format(word, count))
